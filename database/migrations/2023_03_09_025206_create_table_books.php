ebaa2a882f8832d92ad80582e1e494dc455d110d
Nguyen Duy Quoc <nguyenduyquoc129829042001@gmail.com>
2023-03-09 11:08:08 +0700

    submit

---
+++ database/migrations/2023_03_09_025206_create_table_books.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableBooks extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string("title", 55);
+            $table->string("ISBN", 25);
+            $table->smallInteger("pub_year")->default(0);
+            $table->tinyInteger("available");
+            $table->unsignedBigInteger("author_id")->nullable();
+            $table->foreign("author_id")->references("id")->on("authors");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
